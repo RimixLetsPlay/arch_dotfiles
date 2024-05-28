363a398613b78c52d01f49af97984ac1f51a9592
RimixLetsPlay <fkrill@web.de>
2024-05-28 20:36:57 +0200

    better structure

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,3 @@
-require "user.keymaps"
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -13,34 +11,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    -- or                              , branch = '0.1.x',
-          dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        'nvim-treesitter/nvim-treesitter', build = ":TSUpdate"
-    },
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = {"c", "lua"},
-    highlight = { enable = true },
-    indent = { enable = true },
-})
-
-vim.cmd[[colorscheme tokyonight]]
+require("options")
+require("keymaps")
+require("lazy").setup("plugins")
